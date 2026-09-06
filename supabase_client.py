6e5c80ec4d6068ef2edda409c7d684655c331994
AABHAS <adsamishra@gmail.com>
2026-09-06 16:46:00 +0530

    Update Supabase client

--- supabase_client.py
+++ supabase_client.py
@@ -4,8 +4,8 @@ from supabase import create_client
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://qlidfgyxgudykqlvltav.supabase.co")
+SUPABASE_KEY = os.getenv("sb_publishable_w58lPsTZfb0VPeQeipIezw_sWpSMqPL")
 
 if not SUPABASE_URL:
     raise ValueError("SUPABASE_URL is missing from .env")
